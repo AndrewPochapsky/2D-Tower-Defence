abfdffa268cde43a68e0f27c5b91d9cf1c516431
Andrew Pochapsky-LPTP <andrew.pochapsky@gmail.com>
2017-05-30 09:04:24 -0400

    basic scripts and basic raycasting

---
+++ Assets/Scripts/MouseRay.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetMouseButtonDown(0))
+            CastRay();
+	}
+
+    private void CastRay()
+    {
+        print("Casting ray fromo mouse");
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+        /*if (hit.collider.gameObject.GetComponent<TowerLocation>())
+        {
+            Debug.Log("Tower Location");
+
+        }*/
+        if (hit.collider != null)
+        {
+            Debug.Log(hit.collider.gameObject.name);
+         
+        }
+
+    }
+
+
+}
