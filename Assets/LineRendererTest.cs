44fddfc48d2982bf149cf07db1c84f487f66abd6
Andrew Pochapsky-LPTP <andrew.pochapsky@gmail.com>
2017-06-05 10:36:58 -0400

    basic linerenderer created, now need to make a collider at its end

---
+++ Assets/LineRendererTest.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRendererTest : MonoBehaviour {
+    LineRenderer lineRenderer;
+	// Use this for initialization
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lineRenderer.SetPosition(1, new Vector3(0, 20, 0));
+	}
+}
